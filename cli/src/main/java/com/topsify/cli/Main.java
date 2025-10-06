31216ba8eb42b3b7b051c4b33a256671ab5b5f32
SoftwareEngineer101 <tanianazarian125@gmail.com>
2025-10-06 16:30:47 +0100

    Workshop in Progress

--- cli/src/main/java/com/topsify/cli/Main.java
+++ cli/src/main/java/com/topsify/cli/Main.java
@@ -5,20 +5,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 
 public class Main {
 
 
-    public static void main(String[] args) throws IOException {
+    public static <JSONFileReader> void main(String[] args) throws IOException {
 //        System.out.println("Hello World!");
+        Path fileName;
+        fileName = Paths.get("src", "reports/users_json");
 
-       Path path = new Path();
-       path.getFileName("reports", "users.json");
+
 
 
         try {
             ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.readValue()
+            List<User> users = new ArrayList<>();
 
 
         }

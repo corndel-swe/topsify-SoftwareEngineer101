e48f2525cb66d6b21bd1768fedc2dee8b06565ad
SoftwareEngineer101 <tanianazarian125@gmail.com>
2025-10-09 12:18:15 +0100

    Exercises Complete

--- exercises/6b.sql
+++ exercises/6b.sql
@@ -1,7 +1,7 @@
 -- Create a list of all album names
 -- Include the total duration of the album
 -- Sort the results by album duration, with the longest first
-SELECT albums.names SUM (tracks.duration_ms) AS AlbumDuration
+SELECT albums.name SUM (tracks.duration_ms) AS AlbumDuration
 INNER JOIN tracks ON albums.id = tracks.album_id
 GROUP BY albums.id
 ORDER BY AlbumDuration;
